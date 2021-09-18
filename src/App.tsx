8966473abe0ff798c91bd4f040b3079e2f5c403d
Richard Paul <richard@richardnpaul.co.uk>
2021-09-18 15:54:35 +0100

    Update react app to collect and send data to an api (validation on inputs required)

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [name, setName] = useState();
+  const [email, setEmail] = useState();
+  const [msg, setMsg] = useState();
+
+  const submit = (e: React.SyntheticEvent,) => {
+    e.preventDefault();
+    fetch("/api/formdata/", {
+        method: 'POST',
+        cache: 'no-cache',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': 'Basic k9M5BKRGMslwylSiuKMlraG1gYkqZSWW74Uv9THX9obnKiuJ9M',
+        },
+        body: `{"name": "${name}", "email": "${email}", "message": "${msg}"}`,
+      }
+    )
+      .then(res => (res.ok ? res : Promise.reject(res)))
+      .then(res => res.json())
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={submit}>
+        <label>Name:
+          <input name="Name" type="text" value={name} onChange={() => setName(name)}/>
+        </label>
+        <label>Email:
+          <input name="Email" type="email" value={email} onChange={() => setEmail(email)}/>
+        </label>
+        <label>Message:
+          <input name="Message" type="textarea" value={msg} onChange={() => setMsg(msg)}/>
+        </label>
+        <button type="submit" name="submit">Submit</button>
+      </form>
     </div>
   );
 }
