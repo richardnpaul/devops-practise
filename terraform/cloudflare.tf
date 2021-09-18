840b53da45e45a9f089d5ffcb1a7bb2fbf1b090e
Richard Paul <richard@richardnpaul.co.uk>
2021-09-18 17:06:19 +0100

    Remove non-functional data source.  Add https redirect

--- terraform/cloudflare.tf
+++ terraform/cloudflare.tf
@@ -1,15 +1,17 @@
-data "cloudflare_zones" "domain" {
-  filter {
-    name = var.site_domain
-  }
-}
-
 resource "cloudflare_record" "site_cname" {
-  zone_id = data.cloudflare_zones.domain.zones[0].id
+  zone_id = var.cloudflare_zone_id
   name    = var.site_domain
   value   = aws_s3_bucket.app_bucket.website_endpoint
   type    = "CNAME"
 
   ttl     = 1
   proxied = true
+}
+
+resource "cloudflare_page_rule" "https" {
+  zone_id = var.cloudflare_zone_id
+  target  = "*.${var.site_domain}/*"
+  actions {
+    always_use_https = true
+  }
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -8,3 +8,8 @@ variable "site_domain" {
   type        = string
   description = "The domain name to use for the static site"
 }
+
+variable "cloudflare_zone_id" {
+  type = string
+  description = "The zone id from Cloudflare"
+}
