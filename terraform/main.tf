2feb048f695da572f4b7909c6cc7f64a1a803ac6
Richard Paul <richard@richardnpaul.co.uk>
2021-09-19 19:22:41 +0100

    Adding api gateway to main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,9 +1,10 @@
 resource "aws_lambda_function" "lambda_save_to_s3" {
-  role          = aws_iam_role.iam_for_lambda.arn
-  handler       = "lambda.handler"
-  runtime       = "python3.9"
-  filename      = "lambda.zip"
-  function_name = "lambda_function"
+  role             = aws_iam_role.iam_for_lambda.arn
+  handler          = "lambda.handler"
+  runtime          = "python3.9"
+  filename         = "lambda.zip"
+  function_name    = "lambda_function"
+  source_code_hash = filebase64sha256("lambda.zip")
 
   environment {
     variables = {
@@ -11,4 +12,24 @@ resource "aws_lambda_function" "lambda_save_to_s3" {
       auth_token  = var.auth_token
     }
   }
+}
+
+resource "aws_apigatewayv2_api" "lambda-access-endpoint" {
+    name                         = "accept-form-post-data"
+    protocol_type                = "HTTP"
+}
+
+
+resource "aws_apigatewayv2_route" "lambda-access-endpoint" {
+    api_id               = aws_apigatewayv2_api.lambda-access-endpoint.id
+    target               = "integrations/${aws_apigatewayv2_integration.lambda-access-endpoint.id}"
+    route_key            = "POST /api/formdata"
+}
+
+resource "aws_apigatewayv2_integration" "lambda-access-endpoint" {
+    api_id                 = aws_apigatewayv2_api.lambda-access-endpoint.id
+    integration_method     = "POST"
+    integration_type       = "AWS_PROXY"
+    integration_uri        = aws_lambda_function.lambda_save_to_s3.arn
+    payload_format_version = "2.0"
 }
