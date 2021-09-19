789efb577616c556739584c7a71f3d0a62905c77
Richard Paul <richard@richardnpaul.co.uk>
2021-09-19 19:22:05 +0100

    Add gateway endpoint and fix incorrect state setting

--- src/App.tsx
+++ src/App.tsx
@@ -8,14 +8,15 @@ function App() {
 
   const submit = (e: React.SyntheticEvent,) => {
     e.preventDefault();
-    fetch("/api/formdata/", {
+    fetch("https://c63v02a6we.execute-api.eu-west-2.amazonaws.com/api/formdata", {
         method: 'POST',
         cache: 'no-cache',
+        mode: 'no-cors',
         headers: {
           'Content-Type': 'application/json',
           'Authorization': 'Basic k9M5BKRGMslwylSiuKMlraG1gYkqZSWW74Uv9THX9obnKiuJ9M',
         },
-        body: `{"name": "${name}", "email": "${email}", "message": "${msg}"}`,
+        body: `{"name": "${name}", "email": "${email}", "msg": "${msg}"}`,
       }
     )
       .then(res => (res.ok ? res : Promise.reject(res)))
@@ -26,13 +27,13 @@ function App() {
     <div className="App">
       <form onSubmit={submit}>
         <label>Name:
-          <input name="Name" type="text" value={name} onChange={() => setName(name)}/>
+          <input name="Name" type="text" value={name} onChange={(e: any,) => setName(e.target.value)} />
         </label>
         <label>Email:
-          <input name="Email" type="email" value={email} onChange={() => setEmail(email)}/>
+          <input name="Email" type="email" value={email} onChange={(e: any,) => setEmail(e.target.value)} />
         </label>
         <label>Message:
-          <input name="Message" type="textarea" value={msg} onChange={() => setMsg(msg)}/>
+          <input name="Message" type="textarea" value={msg} onChange={(e: any,) => setMsg(e.target.value)} />
         </label>
         <button type="submit" name="submit">Submit</button>
       </form>
