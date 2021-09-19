6aa2ac531298fd3045a1f50ce28fe82c0a66004f
Richard Paul <richard@richardnpaul.co.uk>
2021-09-19 20:46:12 +0100

    Add logging to lambda function

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -50,3 +50,30 @@ resource "aws_iam_role_policy_attachment" "attach_s3_policy_to_lambda_role" {
   role       = aws_iam_role.iam_for_lambda.name
   policy_arn = aws_iam_policy.iam_policy_for_lambda_to_s3.arn
 }
+
+resource "aws_iam_policy" "iam_policy_for_lambda_logging" {
+  name        = "lambda-logging-policy"
+  description = "IAM policy for logging from a lambda"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "logs:CreateLogGroup",
+        "logs:CreateLogStream",
+        "logs:PutLogEvents"
+      ],
+      "Resource": "arn:aws:logs:*:*:*",
+      "Effect": "Allow"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "attach_logging_policy_to_lambda_role" {
+  role       = aws_iam_role.iam_for_lambda.name
+  policy_arn = aws_iam_policy.iam_policy_for_lambda_logging.arn
+}
